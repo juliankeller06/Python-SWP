684005cf59764d23907aa7e58cce486781e5332f
Julian Keller <104064549+juliankeller06@users.noreply.github.com>
2024-09-12 16:14:20 +0200

    Test

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+a = 1
+b = 10
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def hello_world():
+    print("Hello World!")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print_hi('PyCharm')
+    hello_world()
+    if a in range(b):
+        print("valid")
+    else:
+        print("not valid")
+    for a in range(b):
+        print("c")
+    while a in range(b):
+        print("d")
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
